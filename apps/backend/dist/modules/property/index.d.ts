90121ac8a067df9f45da8dd48633baa1068587d2
Miranda Méndez Cruz <miricruz1570@gmail.com>
2026-09-16 03:35:45 -0600

    chore(HAB-13): include session config, backend dist and node_modules

---
+++ apps/backend/dist/modules/property/index.d.ts
@@ -0,0 +1,6 @@
+export * from './domain/entities/catalog-types';
+export * from './domain/entities/room-space';
+export * from './domain/entities/property.entity';
+export * from './domain/entities/listing.entity';
+export * from './domain/repositories/repositories';
+export * from './property.module';
